8947863c0e739cb1d111faccbffad8c0865ae215
Antonio Blescia <a.blescia@nocommentlab.it>
2022-04-01 21:38:25 +0200

    Update create_hedera_observable_types.ps1

--- utils/thihive/create_hedera_observable_types.ps1
+++ utils/thihive/create_hedera_observable_types.ps1
@@ -1,20 +1,117 @@
+function Get-DefaultAdminUid {
+
+    param(
+        [Parameter(Mandatory)]
+        [string] $Ip,
+        
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Credential,
+
+
+        [int] $Port=9000
+
+    )
+
+    $adminUid = $null
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Content-Type", "application/json")
+
+    $body = "{`"query`": [{`"_name`": `"listOrganisation`"},{`"_name`": `"users`"}]}"
+
+    try {
+        $deserializedResponse = Invoke-RestMethod "http://${Ip}:$Port/api/v1/query?name=users" -Method 'POST' -Headers $headers -Body $body -Credential $Credential
+        $adminUid = $($deserializedResponse | Where-Object {$_.Profile -eq "admin" -and $_._createdBy -eq "system@thehive.local"})._id
+
+        Write-Host "[OK]  - Admin UID retrieved: ${adminUid}" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "[NOK] - Error during retrieve Admin UID: ${adminUid}" -ForegroundColor Green
+    }
+    
+    return $adminUid
+    
+    
+}
+
+function Set-DefaultAdminPassword {
+
+    param(
+        [Parameter(Mandatory)]
+        [string] $Ip,
+        
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Credential,
+
+        [Parameter(Mandatory)]
+        [string] $Uid,
+
+        [int] $Port=9000
+
+    )
+
+    $newPasswordSecureString = Read-Host "Insert the new password" -AsSecureString
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Content-Type", "application/json")
+
+    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($newPasswordSecureString)
+    $newPasswordString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+    $body = "{`"password`": `"${newPasswordString}`"}"
+
+    try {
+        Invoke-RestMethod "http://${Ip}:$Port/api/v1/user/$Uid/password/set" -Method 'POST' -Headers $headers -Body $body -Credential $Credential | Out-Null
+        Write-Host "[OK]  - Admin password changed" -ForegroundColor Green
+    }catch{
+        Write-Host "[NOK] - Admin password not changed" -ForegroundColor Red
+    }
+    
+}
+
 function Add-HederaObservables {
 
     param(
-        [Parameter(Mandatory, Position=0)]
-        [string] $APIKey
+        [Parameter(Mandatory)]
+        [string] $Ip,
+        
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Credential,
+
+
+        [int] $Port=9000
+
     )
 
-    $HederaObservables = @("registry","file","process","pipe")
+    $HederaObservables = @("registry", "file", "process", "pipe")
+
+
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Content-Type", "application/json")
 
     $HederaObservables | ForEach-Object {
-        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-        $headers.Add("Authorization", "Bearer lx9RWX4B1+CrsHkZqlqDMVUkIcspfEJE")
-        $headers.Add("Content-Type", "application/json")
+        
+        try {
 
-        $body = "{`n    `"title`": `"My first case`",`n    `"description`": `"This case has been created by my custom script`"`n    `"tasks`": [{`n        `"title`": `"mytask`",`n        `"description`": `"description of my task`"`n    }],`n    `"customFields`": {`n        `"cvss`": {`n            `"number`": 9,`n        },`n        `"businessImpact`": {`n            `"string`": `"HIGH`"`n        }`n    }`n}"
-
-        $response = Invoke-RestMethod 'http://192.168.1.64:9000/api/case' -Method 'POST' -Headers $headers -Body $body
-        $response | ConvertTo-Json
+            $body = "{`"name`":`"$_`"}"
+            Invoke-RestMethod "http://${Ip}:$Port/api/observable/type" -Method 'POST' -Headers $headers -Body $body -Credential $Credential | Out-Null
+            Write-Host "[OK]  - Observable type $_ created" -ForegroundColor Green
+            
+        }
+        catch {            
+            $deserializedResponse = ConvertFrom-Json -InputObject $_
+            Write-Host "[NOK] - $($deserializedResponse.message)" -ForegroundColor Red
+        }
+        
     }
 }
+
+
+
+$creds = Get-Credential -Message 'Enter default thehive credential'
+
+$adminId = Get-DefaultAdminUid -IP 192.168.1.70 -Credential $creds
+
+Set-DefaultAdminPassword -IP 192.168.1.70 -Credential $creds -Uid $adminId
+
+#Add-HederaObservables -IP 192.168.1.64 -Credential $creds
