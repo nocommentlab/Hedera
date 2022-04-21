f11ea8b16708aa247616a90a1bedb36aaf743939
Antonio Blescia <90195154+ablescia@users.noreply.github.com>
2022-04-21 23:24:04 +0200

    Registry key search (#32)
    * fixed minor bugs
    
    * updated ioc.yaml

--- HederaLib/Models/Configuration/RegistryIndicator.cs
+++ HederaLib/Models/Configuration/RegistryIndicator.cs
@@ -5,10 +5,11 @@ namespace ncl.hedera.HederaLib.Models.Configuration
     {
         public string BaseKey { get; init; }
         public string Key { get; set; }
+        public string Name { get; set; }
         public string ValueName { get; init; }
         public string ValueData { get; init; }
         public bool IsRecursive { get; init; }
-        
+
 
     }
 }
